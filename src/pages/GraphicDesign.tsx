bc41084220078ad85d83bb15ed130e724d21f332
Alice <alicepolishchuk1@gmail.com>
2024-11-24 09:31:43 +0200

    Finished layout and logic for category inner pages and its content

--- src/pages/GraphicDesign.tsx
+++ src/pages/GraphicDesign.tsx
@@ -1,13 +1,31 @@
 import CategoryCard from "../components/CategoryCard.tsx";
 import CategoryTitle from "../components/CategoryTitle.tsx";
+import ProjectCard from "../components/ProjectCard.tsx";
 
 const GraphicDesign = () => {
     return (
         <div className={'category-page'}>
-            <CategoryTitle title={'Graphic Design'} description={"We deliver eye-catching branding materials that are tailored to meet your business objectives."} image={{src: "/images/graphic-design/graphicdesign-pattern.svg",mobSrc: "/images/mob-pattern.svg", alt: "background"}}/>
+            <CategoryTitle title={'Graphic Design'}
+                           description={"We deliver eye-catching branding materials that are tailored to meet your business objectives."}
+                           image={{
+                               src: "/images/graphic-design/graphicdesign-pattern.svg",
+                               mobSrc: "/images/mob-pattern.svg",
+                               alt: "background"
+                           }}/>
+            <div className="projects-wrapper">
+                <ProjectCard title={'Tim Brown'} description={'A book cover designed for Tim Brown’s new release, ‘Change’'}
+                             image={{src: "/images/graphic-design/TimBrown.jpg", alt: "TimBrown"}}/>
+                <ProjectCard title={'BOXED WATER'}
+                             description={'A simple packaging concept made for Boxed Water'}
+                             image={{src: "/images/graphic-design/BoxedWater.jpg", alt: "BoxedWater"}}/>
+                <ProjectCard title={'SCIENCE!'}
+                             description={'A poster made in collaboration with the Federal Art Project'}
+                             image={{src: "/images/graphic-design/SCIENCE!.jpg", alt: "Photon"}}/>
+            </div>
             <div className="categories categories-inner">
                 <CategoryCard link={'/web-design'} backgroundImage={"/images/web-design/webdesign.png"}/>
-                <CategoryCard link={'/app-design'} title={"APP DESIGN"} backgroundImage={"/images/app-design/appdesign.png"}/>
+                <CategoryCard link={'/app-design'} title={"APP DESIGN"}
+                              backgroundImage={"/images/app-design/appdesign.png"}/>
             </div>
         </div>
     );
