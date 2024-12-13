53e2f8fa3e4f76f458175ec311f6f52a94f40354
Alice <alicepolishchuk1@gmail.com>
2024-12-13 21:16:23 +0200

    leaflet marker fix in production

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,16 @@
 import "./map.scss"
 import 'leaflet/dist/leaflet.css'
 import {MapContainer, Marker, TileLayer} from 'react-leaflet'
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 type mapProps = {
     x: number,
